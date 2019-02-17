7bf72cb03e1026d51e859109139b18b5e22d8b70
Your Name <you@example.com>
2019-02-17 21:44:48 +0600

    thymeleaf fragment

---
+++ demofragments/src/main/java/com/beskilled/HomeController.java
@@ -0,0 +1,20 @@
+package com.beskilled;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping(value = "/i")
+    public String index(){
+        return "index";
+    }
+    @GetMapping(value = "/")
+    public String indext(){
+        return "layout";
+    }
+    @GetMapping(value = "/t")
+    public String test(){
+        return "test";
+    }
+}
---
+++ meetingcall/src/main/java/com/beskilled/controller/HomeController.java
@@ -0,0 +1,26 @@
+package com.beskilled.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping(value = "/")
+    public String displayHome(){
+
+        return "index";
+    }
+    @GetMapping(value = "/table")
+    public String tabledisplay(){
+
+        return "table";
+    }
+    @GetMapping(value = "/lay")
+    public String layoutttt(){
+
+        return "layout";
+    }
+
+
+}
