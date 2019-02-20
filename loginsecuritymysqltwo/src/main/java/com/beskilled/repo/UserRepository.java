4a0127c8f8e476d38f028d467bdbe6436f657ab9
mostafiz9900 <mostafiz9900@gmail.com>
2019-02-20 13:01:49 +0600

    temp final

--- loginsecuritymysqltwo/src/main/java/com/beskilled/repo/UserRepository.java
+++ loginsecuritymysqltwo/src/main/java/com/beskilled/repo/UserRepository.java
@@ -10,4 +10,5 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User>  findByUsernameOrEmail(String username, String email);
     User findByUsername(String username);
+    User findByUsernameOrPassword(String username, String password);
 }
