923610ece5e12195c34505486a6de87b04c3e574
Your Name <you@example.com>
2019-02-22 20:22:29 +0600

    finalfragement

---
+++ jewelertemplatefragmentthymeleaf/src/main/java/com/beskilled/JewelertemplatefragmentthymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.beskilled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelertemplatefragmentthymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JewelertemplatefragmentthymeleafApplication.class, args);
+    }
+
+}
---
+++ jewelertemplatefragmentthymeleaf/src/main/java/com/beskilled/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.beskilled.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping(value = "/")
+    public String index(){
+        return "index";
+    }
+    @GetMapping(value = "/la")
+    public String layout(){
+        return "layout";
+    }
+}
